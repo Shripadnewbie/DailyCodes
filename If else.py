51bd2bf592d08770e88783e7e9f8efb2301d9271
Shripadnewbie <cupday91>
2019-09-23 17:28:31 +0530

    Minimum of list tryout

---
+++ If else.py
@@ -0,0 +1,5 @@
+#Minimum of two numbers
+values = input("Enter two numbers separated by commas : ")
+list = values.split(",")
+tuple=(list)
+print(min(list))
--- MinOftwoNumbers.py
+++ MinOftwoNumbers.py
@@ -1,5 +1,8 @@
-#Minimum of two numbers
-values = input("Enter two numbers separated by commas : ")
-list = values.split(",")
-tuple=(list)
+#Minimum of given numbers
+values = input("Enter two numbers separated by either by a space or a comma : ")
+if values[str.find(values,',')] == ',':
+    list = map(int,values.split(","))
+elif values[str.find(values,' ')] == ' ':
+    list = map(int,values.split(" "))
 print(min(list))
+
