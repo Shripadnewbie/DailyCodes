3f4a90374b03a75e408ebcf6155db8d91ed9f159
Shripadnewbie <cupday91>
2019-09-24 16:42:32 +0530

    Updated files with all solutions

--- Snakify_Solutions_If_else_Problems.py
+++ Snakify_Solutions_If_else_Problems.py
@@ -84,7 +84,7 @@ else: print("NO")
         print('NO')
 #Leap_year_Problem
     year = int(input())
-    if year % 4 == 0 and year % 100 == 0 or year % 400 == 0:
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         print('LEAP')
     else:
         print('COMMON')
