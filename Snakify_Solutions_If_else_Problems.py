c975eea6ba9cb25ebb281adcdb036dca9108bf0c
Shripadnewbie <cupday91>
2019-09-24 16:39:31 +0530

    Updated files with all solutions

--- Snakify_Solutions_If_else_Problems.py
+++ Snakify_Solutions_If_else_Problems.py
@@ -82,7 +82,7 @@ else: print("NO")
         print('YES')
     else:
         print('NO')
-#Leap_year
+#Leap_year_Problem
     year = int(input())
     if year % 4 == 0 and year % 100 == 0 or year % 400 == 0:
         print('LEAP')
