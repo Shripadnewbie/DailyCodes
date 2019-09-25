b1bfb4cee319a889f127f9f2193dd49d0ea09c9e
Shripadnewbie <cupday91>
2019-09-25 12:22:41 +0530

    Updated files with all solutions

---
+++ Guess Game.py
@@ -0,0 +1,12 @@
+secretWord = 'Mello'
+guess = ''
+counter = 0
+left = 3
+while guess != secretWord and left != 0:
+    guess = input('Enter your guess below '+ "You have "+ str(left) + " number of guesses left\n :")
+    counter = counter+1
+    left = left -1
+if counter == 3 and guess != secretWord:
+    print('Out of guesses.You Lose!!')
+elif counter <= 3 and guess == secretWord:
+    print('You win')
